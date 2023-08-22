c455ef6ebdd3a1e919b0f36efc1a9f47c0195361
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-08-22 09:07:32 -0500

    webpack config cli needs to be set for reload

--- 03-Chart.js/webpack.config.js
+++ 03-Chart.js/webpack.config.js
@@ -2,6 +2,10 @@ const path = require('path');
 
 module.exports = {
   entry: '/src/index.js',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    liveReload: true, // Automatically refresh when changes occur
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
